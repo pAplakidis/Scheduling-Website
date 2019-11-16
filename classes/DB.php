0a24173c9e750e26387ceb3799c85a68524475a6
Unknown <aplakidisp@gmail.com>
2019-11-16 17:43:00 +0200

    finished DB.php, replaced index.php
    index.html contains links and is a welcome page with description of the project

--- classes/DB.php
+++ classes/DB.php
@@ -9,5 +9,15 @@ class DB
         
         return $pdo;
     }
+
+    public static function query($query, $params = array()){
+		$statement = self::connect()->prepare($query);
+		$statement->execute($params);
+
+		if(explode(' ', $query)[0] == 'SELECT'){
+		$data = $statement->fetchAll();
+		return $data;
+		}
+	}
 }
 ?>
