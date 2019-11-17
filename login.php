16ae03a3b47e6a1cad251100a0b3d82a04d19376
Unknown <aplakidisp@gmail.com>
2019-11-17 19:22:13 +0200

    created login.php and register.php
    basic form and file inclusion

---
+++ login.php
@@ -0,0 +1,11 @@
+<?php
+include('classes/DB.php');
+
+?>
+
+<h1>Log In</h1>
+<form class="login.php" method="post">
+    <input type="text" name="username" placeholder="Username"><p />
+    <input type="password" name="password" placeholder="Password"><p />
+    <input type="submit" name="register" value="Log In">
+</form>
---
+++ register.php
@@ -0,0 +1,11 @@
+<?php
+include('classes/DB.php');
+
+?>
+
+<h1>Register</h1>
+<form class="register.php" method="post">
+    <input type="text" name="username" placeholder="Username"><p />
+    <input type="password" name="password" placeholder="Password"><p />
+    <input type="submit" name="register" value="Register">
+</form>
