6e721d2585e92b8e0534406356669c902b2b5ab2
Efthimis <efthimis-jr@outlook.com.gr>
2020-09-07 20:35:08 +0300

    ειναι φουλ προχειρο να ξες
    τσεκαρε τα μια να δεις αμα σου εστειλα τα σωστα

---
+++ classes/schedule_script.js
@@ -0,0 +1,49 @@
+const draggables = document.querySelectorAll('.draggable');
+const empties = document.querySelectorAll('.empty');
+
+let draggedDraggable = null;
+
+//Items Listeners
+for(let i = 0; i < draggables.length; i++){
+    const draggable = draggables[i];
+
+    item.addEventListener('dragstart', function(){
+     draggedDraggable = item;
+     setTimeout(function(){
+            item.style.display = 'none';
+        }, 0);  
+    });
+
+    item.addEventListener('dragend', function(){
+        setTimeout(function(){
+         //this.className = 'draggable';
+         draggedDraggable.style.display = 'block;'
+         draggedDraggable = null;
+        }, 0);
+    });
+
+    for(let j = 0; j < empties.length; j++){
+        const empty = empties[j];
+
+     empty.addEventListener('dragover', function(e){
+         e.preventDefault();
+      });
+
+     empty.addEventListener('dragenter', function(e){
+          e.preventDefault();
+        });
+
+     empty.addEventListener('dragleave', function(){
+
+         });
+
+     empty.addEventListener('drop', function(){
+            console.log('drop');
+            this.append(draggedDraggable);
+         });
+    }
+}
+
+
+
+
